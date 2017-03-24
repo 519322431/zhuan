6ed453ec271d48efec0218f18ad186642c8f090d
519322431 <519322431@qq.com>
2017-03-24 11:43:36 +0800

    zhuan

---
+++ js/js.js
@@ -0,0 +1,19 @@
+// window.onload=function(){
+// 	var canvas=document.getElementById('canvas');
+// 	var context=canvas.getContext('2d');
+// 	context.beginPath();
+// 	context.moveTo(100,100);
+// 	context.lineTo(700,700);
+// 	context.lineTo(700,0);
+// 	context.lineTo(100,100);
+// 	context.closePath();
+// 	context.lineWidth=5;
+// 	context.strokeStyle="red";
+// 	context.stroke();
+// 	context.beginPath();
+// 	context.moveTo(200,100);
+// 	context.lineTo(700,600);
+// 	context.closePath();
+// 	context.strokeStyle="#000";
+// 	context.stroke();
+// }
